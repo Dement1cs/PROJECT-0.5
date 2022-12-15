989235a515c3a432ced3742afdf1fe848713a9a1
Dement1cs <93581822+Dement1cs@users.noreply.github.com>
2022-12-15 11:32:48 +0000

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,39 @@
+function hide(){
+    document.getElementById('dima').style.display="hidden"
+    document.getElementById('dima').innerHTML="text"
+    document.getElementById('dima').style.fontSize="30"
+    document.getElementById('dima').style.width="700"
+    document.getElementById('dima').style.height="467"
+    document.getElementById('dima').style.textAlign="left"
+    document.getElementById('dima').style.backgroundColor="white"
+}
+
+function hide2(){
+    document.getElementById('dima2').style.display="hidden"
+    document.getElementById('dima2').innerHTML="text"
+    document.getElementById('dima2').style.fontSize="30"
+    document.getElementById('dima2').style.width="700"
+    document.getElementById('dima2').style.height="467"
+    document.getElementById('dima2').style.textAlign="left"
+    document.getElementById('dima2').style.backgroundColor="white"
+}
+
+function hide3(){
+    document.getElementById('dima3').style.fontSize="30"
+    document.getElementById('dima3').style.display="hidden"
+    document.getElementById('dima3').innerHTML="text"
+    document.getElementById('dima3').style.width="700"
+    document.getElementById('dima3').style.textAlign="left"
+    document.getElementById('dima3').style.backgroundColor="white"
+    document.getElementById('dima3').style.height="467"
+}
+
+function hide4(){
+    document.getElementById('dima4').style.fontSize="30"
+    document.getElementById('dima4').style.display="hidden"
+    document.getElementById('dima4').innerHTML="text"
+    document.getElementById('dima4').style.width="700"
+    document.getElementById('dima4').style.textAlign="left"
+    document.getElementById('dima4').style.backgroundColor="white"
+    document.getElementById('dima4').style.height="467"
+}
